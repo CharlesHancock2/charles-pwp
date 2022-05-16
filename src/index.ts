01b7d478d0516b558660213620bd577bbd067d73
Charles Hancock <hhancock@cnm.edu>
2022-05-16 15:20:22 -0600

    added index.ts

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import { Application } from 'express'
+import {Request, Response} from 'express'
+
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const Recaptcha = require('express-recaptcha').RecaptchaV2
+const formData = require('form-data')
+const Mailgun = require('mailgun.js')
+const mailgun = new Mailgun(formData)
+const {check, validationResult} = require('express-validator')
+require('dotenv').config()
+
+const validation = [
+    check('name', 'A valid name is required.').not().isEmpty().trim().escape(),
+    check('email', 'Please provide a valid email address.').isEmail(),
+    check('message', 'A message of up to 2000 characters is required.').trim().escape().isLength({min: 1, max:2000})
+]
+
+
+const app: Application = express()
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+const recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY)
+
+const mailgunClient = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY})
+
+const handleGetRequest = (request: Request, response: Response) => {
+    return response.json('This thing is on!')
+}
+
+const indexRoute = express.Router()
+
+indexRoute.route('/')
+    .get(handleGetRequest)
+
+app.use('/apis', indexRoute)
+
+app.listen(4200, () => {
+    console.log('Express Successfully Built')
+})
